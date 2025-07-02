f7c506709c135aacec37bf2e8a86048d6d5186b3
Vedang Doley <Vedang Doley@LAPTOP-J08D7O96>
2025-07-02 09:43:31 +0530

    updated code

--- Day1/src/looping_statements/ForLoopDemo.java
+++ Day1/src/looping_statements/ForLoopDemo.java
@@ -3,7 +3,7 @@ package looping_statements;
 public class ForLoopDemo {
 
 	public static void main(String[] args) {
-		for(int i=100;i>=1;i++) {
+		for(int i=100;i>=1;i--) {
 			System.out.println("Value of i is"+i);
 		}
 	}
